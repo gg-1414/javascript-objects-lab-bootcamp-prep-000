4095a3b63c61b7f52cf02ab377054522471acff7
Gina Lee <ginalee1114@gmail.com>
2018-06-24 02:18:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value) {
   var newObject = Object.assign({}, object, { [key]: value });
   return newObject;
-  return object;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
