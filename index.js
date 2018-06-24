785bb0a198829428273ee5eb370d2c91957972d8
Gina Lee <ginalee1114@gmail.com>
2018-06-24 02:21:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  delete 
+  var newObject = Object.assign({}, object);
+  delete newObject.key;
+  return newObject;
 }
